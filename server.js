c0faf8fcc5ad818089445161fcf96ba00d482f57
Okens <Ng.Eric314@gmail.com>
2018-06-06 21:00:12 -0700

    Adding dotenv config, and declaring app = express().

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
+
+var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
