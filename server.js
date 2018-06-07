65aebcbdc44d693b0bcbc45ed718de17e0227293
Okens <Ng.Eric314@gmail.com>
2018-06-06 20:38:45 -0700

    Setting up server js with PORT, express, bodyParser, route, and listening on PORT 8080.

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine","handlebars");
+
+var routes = require("./controllers/pokemon_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Listening on localhost:" + PORT);
+});
