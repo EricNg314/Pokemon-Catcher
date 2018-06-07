f42bdc1c0f5edcad02c8b45582f8af9ef8845455
Okens <Ng.Eric314@gmail.com>
2018-06-07 01:22:56 -0700

    Adding update query for when pokemon is caught.

--- config/orm.js
+++ config/orm.js
@@ -12,6 +12,25 @@ function printQuestionMarks(num) {
 }
 
 
+function objToSql(ob){
+    var arr = [];
+
+    for (var key in ob){
+        var value = ob[key];
+
+        //Skipping hidden properties.
+        if(Object.hasOwnProperty.call(ob,key)){
+            //Adding quotes if string has spaces.
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+
 var orm = {
     selectAll: function (tableInput, cb) {
         console.log("Select All ORM");
@@ -46,7 +65,20 @@ var orm = {
     },
     updateOne: function (table, objColVals, condition, cb) {
         console.log("Update One ORM");
-    },
-}
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result){
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
 
 module.exports = orm;
