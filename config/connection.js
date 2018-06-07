2a614f37b518e45b1fdd1cece8897e2e6a9ea821
Okens <ng.eric314@gmail.com>
2018-06-07 14:27:08 -0700

    Adding JAWSDB connection.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_KEY,
-    database: "pokemon_db"
-})
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+        connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_KEY,
+        database: "pokemon_db"
+    });
+};
 
 connection.connect(function(err){
     if(err){
