975f46fc05ffc942cc03c34e11540cc902134bf8
Okens <Ng.Eric314@gmail.com>
2018-06-07 00:41:18 -0700

    Linking catch button to the pokemon_html.js

--- public/assets/js/pokemon_html.js
+++ public/assets/js/pokemon_html.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
     console.log("testing1");
-    $(".create-form").on("submit", function(event){
+    $(".create-form").on("submit", function (event) {
         event.preventDefault();
         console.log("testing2");
         var newPoke = {
@@ -11,14 +11,25 @@ $(function(){
             type: "POST",
             data: newPoke
         }).then(
-            function(){
+            function () {
                 console.log("creating new pokemon");
                 location.reload();
             }
         )
+    });
+
+    $(".catch-pokemon").on("click", function (event) {
+        console.log("testing3");
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var newCaught = $(this).data("newcaught");
+
+        // console.log(id)
+
+
+
     })
-
-
 
 
 
